ecbc1434c661d560fdd23a2f7acb0d20896f8fcf
Nastya <a.dernova@ukma.edu.ua>
2024-03-21 21:37:45 +0200

    Program that convert numbers from 0 to 9 and more into binary (there are some defects)

--- VSCode.asm
+++ VSCode.asm
@@ -1,43 +1,41 @@
 .model small
 .stack 100h
 .data
-      msg db 'Enter the Decimal Number: $'
-      msg1   db 0dh,0ah,'Binary Number: $'
+    msg         db 'Binary Number: $'
+    decimalNum  dw -1    ; Example decimal number to convert
+
 .code
-       main proc
-            mov ax,@data
-            mov ds,ax
+main proc
+    mov ax, @data
+    mov ds, ax
 
-            mov ah,09h
-            lea dx,msg
-            int 21h
+    mov ax, decimalNum          ; Load the decimal number to convert
+    mov bx, 2                   ; Divisor for binary conversion
+    mov cx, 16                  ; Maximum number of digits
 
-            mov ah,01h
-            int 21h
-            sub al,48
-            mov ah,0
-            mov bx,2
-            mov dx,0
-            mov cx,0
-        again:
-            div bx
-            push dx
-            mov ah,0
-            inc cx
-            cmp ax,0
-            jne again
+    sub sp, 16                  ; Allocate space on the stack for binary digits
 
-            mov ah,09h
-            lea dx,msg1
-            int 21h
-        disp:
-            pop dx
-            add dx,48
-            mov ah,02h
-            int 21h
-            loop disp
+binaryLoop:
+    xor dx, dx                  ; Clear dx for division
+    div bx                      ; Divide by 2
+    add dl, '0'                 ; Convert remainder to ASCII
+    push dx                     ; Push binary digit onto the stack
+    dec cx                      ; Decrement loop counter
+    test ax, ax                 ; Check if quotient is zero
+    jnz binaryLoop              ; If not, continue looping
 
-            mov ah,4ch
-            int 21h
-            main endp
-        end main
+    mov ah, 09h                 ; Display string function
+    lea dx, msg                 ; Load message
+    int 21h                     ; Display message
+
+printBinaryLoop:
+    pop dx                      ; Pop binary digit from the stack
+    mov ah, 02h                 ; Display character function
+    int 21h                     ; Display binary digit
+    loop printBinaryLoop        ; Loop until all digits are printed
+
+    mov ah, 4ch                 ; Exit program
+    int 21h
+
+main endp
+end main
